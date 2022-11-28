9cad94e9cd7d451c9991a6fd9bc336970137eb4f
Lidvin Bence <lidvin.bence@students.jedlik.eu>
2022-11-28 10:50:44 +0100

    Áruk törlése

--- functions.py
+++ functions.py
@@ -28,12 +28,19 @@ def aruKiiras():
     print("-----------------Az összes áru--------------")
     for i in range(0, len(aruk)):
         print(f"\t{i+1}. {(aruk[i])}: {arak[i]} Ft ")
-    input("")
 
-def mentesFajlba(aru, ar):
+def ujAruFajlVegere(aru, ar):
     file=open("aruk.csv", "a", encoding="utf-8")
     file.write(f"\n{aru};{ar}")
     file.close()
+
+def mentesFajlba():
+    file=open("aruk.csv", "w", encoding="utf-8")
+    for i in range(len(aruk)):
+        file.write(f"{aruk[i]}; {arak[i]}")
+        if i<len(aruk)-1:
+            file.write("\n")
+    file.close() 
 
 def ujAru():
     system("cls")
@@ -42,7 +49,17 @@ def ujAru():
     ujAr=int(input("Áru ára==>"))
     aruk.append(ujAru)
     arak.append(ujAr)
-    mentesFajlba(ujAru, ujAr)
+    ujAruFajlVegere(ujAru, ujAr)
     input("A felvétel megtörtént...")
+
+def aruTorlese():
+    system("cls")
+    print("---------Áru törlése----------")
+    aruKiiras()
+    sorszam=int(input("Kérem adja meg a törölni kívánt ember sorszámát==>"))
+    aruk.pop(sorszam-1)
+    arak.pop(sorszam-1)
+    mentesFajlba()
+    input("A törlés sikerült...")
 
         
--- main.py
+++ main.py
@@ -8,10 +8,11 @@ while valasztas!=0:
     valasztas=menu()
     if valasztas=="1":
         aruKiiras()
+        input("")
     if valasztas=="2":
         ujAru()
     if valasztas=="3":
-        pass
+        aruTorlese()
     if valasztas=="4":
         pass
     if valasztas=="5":
